6c9ec897a698e03e12ce704806bd1901d4328543
zhaoyd5268 <zhaoyd5268@gmail.com>
2023-01-14 11:23:00 -0800

    Finish LogSearch(Level 3, Module 1, Project 7 of 10)

--- src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_06_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -1,8 +1,79 @@
 package _06_Intro_To_Hash_Maps;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class _02_LogSearch {
-    /*
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class _02_LogSearch implements ActionListener {
+	HashMap <Integer, String> stuff = new HashMap <Integer, String> ();
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+    JButton AddEntry = new JButton();
+	JButton IDSearch = new JButton();
+	JButton ListView = new JButton();
+	public void start() {
+		frame.add(panel);
+		panel.add(AddEntry);
+		panel.add(IDSearch);
+		panel.add(ListView);
+		AddEntry.setText("Add Entry");
+		IDSearch.setText("Search by ID");
+		ListView.setText("View List");
+		AddEntry.addActionListener(this);
+		IDSearch.addActionListener(this);
+		ListView.addActionListener(this);
+		frame.setVisible(true);
+	
+	}
+
+
+	
+	public static void main(String[] args) {
+		_02_LogSearch log = new _02_LogSearch();
+		log.start();
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton button = (JButton) e.getSource();
+		if (button.equals(AddEntry)) {
+			String IDnum = JOptionPane.showInputDialog("Please enter an ID number");
+			Integer IDasInt = Integer.parseInt(IDnum);
+			String name = JOptionPane.showInputDialog("Please enter a name");
+			stuff.put(IDasInt, name);
+		}
+		if (button.equals(IDSearch)) {
+			String IDnum = JOptionPane.showInputDialog("Please enter an ID number");
+			Integer IDasInt = Integer.parseInt(IDnum);
+			if (stuff.containsKey(IDasInt)) {
+				JOptionPane.showMessageDialog(null, "The name is " + stuff.get(IDasInt));
+			} else {
+				JOptionPane.showMessageDialog(null, "That name does not exist. Please check for typos or enter a different ID");
+			}
+
+		}
+		
+		if (button.equals(ListView)) {
+		for(Integer i : stuff.keySet()) {
+			JOptionPane.showMessageDialog(null, "ID:  " + i + "  Name:  " + stuff.get(i) );
+		}
+		
+			
+		}
+	}
+	
+	
+	
+	/*		
      * Crate a HashMap of Integers for the keys and Strings for the values.
      * Create a GUI with three buttons.
      * Button 1: Add Entry
